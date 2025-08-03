c90a14601529886e4a472492612f0f7d0ce6b8e6
Tsholofelo13 <tsholo2000tee@gmail.com>
2025-08-03 02:56:01 +0200

    made changes

--- academicratingsys/ratingsys/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("capture_marks/", views.captureMarks, name="capture_marks"),
-    path("capture_details/", views.captureDetails, name="capture_details"),
-    path("lectureview/", views.lectureHomescreen, name = "lectureview")
-]
